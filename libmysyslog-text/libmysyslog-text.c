15a6a9adc5afac737131778efb616dd9c36eab20
AceFromDungeon <kia01062005@mail.ru>
2024-12-26 17:17:26 +0300

    Update libmysyslog-text.c

--- libmysyslog-text/libmysyslog-text.c
+++ libmysyslog-text/libmysyslog-text.c
@@ -6,3 +6,22 @@ text_log (const char *message, int level, const char *log_path)
 {
     return mysyslog (message, level, /* driver */ 0, /* format */ 0, log_path);
 }
+/*
+//Пример вызова text_log
+int main(void)
+{
+    if (text_log("Hellow!, World!", LOG_INFO, "logfile.txt") != 0)
+    {
+        fprintf(stderr, "Не удалось записать в лог-файл.\n");
+        return 1;
+    }
+
+    if (text_log("Ошибка приложения", LOG_ERROR, "logfile.txt") != 0)
+    {
+        fprintf(stderr, "Не удалось записать в лог-файл.\n");
+        return 1;
+    }
+
+    return 0;
+}
+*/
