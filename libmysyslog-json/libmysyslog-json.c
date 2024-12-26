a245a486af4ce537817d39caa641647b4caa62dc
AceFromDungeon <kia01062005@mail.ru>
2024-12-26 17:17:14 +0300

    Update libmysyslog-json.c

--- libmysyslog-json/libmysyslog-json.c
+++ libmysyslog-json/libmysyslog-json.c
@@ -3,22 +3,4 @@
 int json_log(const char* msg, int level, const char* path) {
 	return mysyslog(msg, level, 1, 1, path);
 }
-/*
-//Пример вызова text_log
-int main(void)
-{
-    if (text_log("Hellow!, World!", LOG_INFO, "logfile.txt") != 0)
-    {
-        fprintf(stderr, "Не удалось записать в лог-файл.\n");
-        return 1;
-    }
 
-    if (text_log("Ошибка приложения", LOG_ERROR, "logfile.txt") != 0)
-    {
-        fprintf(stderr, "Не удалось записать в лог-файл.\n");
-        return 1;
-    }
-
-    return 0;
-}
-*/
